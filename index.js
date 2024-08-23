926a68f33c6aae8fbd70a959f3b3d458ffabff3f
László Monda <laci@monda.hu>
2024-08-23 14:55:52 +0200

    Use aplay because node-web-audio-api is unreliable.

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 #!/usr/bin/env node
 const volume = require('./build/Release/volume.node');
-const nodeWebAudioApi = require('node-web-audio-api');
+const {spawn} = require('child_process');
+
+let childProcess, oldFreq, freq;
 
 async function main() {
-    const AudioContext = nodeWebAudioApi.AudioContext;
-    const context = new AudioContext();
-
-    const lowFreqOscillator = context.createOscillator();
-    lowFreqOscillator.frequency.setValueAtTime(19000, context.currentTime);
-
-    const hiFreqOscillator = context.createOscillator();
-    hiFreqOscillator.frequency.setValueAtTime(20913, context.currentTime);
-
-    hiFreqOscillator.start();
-    lowFreqOscillator.start();
-
-    let oscillator, prevOscillator;
     let vol, prevVol;
     while (true) {
         vol = volume.getVolume();
         if (vol !== prevVol) {
             console.log(`${vol}% volume`);
+            prevVol = vol;
         }
-        prevVol = vol;
 
-        oscillator = vol < 25 ? lowFreqOscillator : hiFreqOscillator;
-        if (oscillator !== prevOscillator) {
-            if (prevOscillator) {
-                prevOscillator.disconnect(context.destination);
+        freq = vol < 25 ? '19000' : '20700';
+        if (freq !== oldFreq) {
+            if (childProcess) {
+                childProcess.kill();
             }
-            oscillator.connect(context.destination);
-            prevOscillator = oscillator;
+
+            childProcess = spawn('aplay', [`${freq}.wav`]);
+            oldFreq = freq;
         }
         await new Promise(resolve => setTimeout(resolve, 500));
     }
